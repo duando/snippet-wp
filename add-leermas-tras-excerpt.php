3541ded3517ee27418f4657522f1d54e0aba0fe9
duando <duando#protonmail.com>
2022-03-19 21:27:57 +0000

    limpieza del código para hacerlo más legible, no se cambia nada más

--- add-leermas-tras-excerpt.php
+++ add-leermas-tras-excerpt.php
@@ -1,17 +1,17 @@
 <?php
 /**
-     * Añadir puntos suspensivos al extracto and mostrar un botón "Leer Más" .
-     * 
-     * @param type $text
-     * @return string
-     */
-	function custom_excerpt($text) {
-		//$text= substr_replace($text,"...",strpos($text, "</p>"),0);
-		$excerpt = $text . '<a href="' . get_permalink() . '">
-		<button class="du-read-more-btn" type="button" value="read_more">Leer más...</button></a>';
-		return $excerpt;
-		}
+    * Añadir puntos suspensivos al extracto and mostrar un botón "Leer Más" .
+    * 
+    * @param type $text
+    * @return string
+    */
+    function custom_excerpt($text) {
+        //$text= substr_replace($text,"...",strpos($text, "</p>"),0);
+        $excerpt = $text . '<a href="' . get_permalink() . '">
+        <button class="du-read-more-btn" type="button" value="read_more">Leer más...</button></a>';
+        return $excerpt;
+    }
 	
-        add_filter('the_excerpt', 'custom_excerpt');
-        
+     add_filter('the_excerpt', 'custom_excerpt');
+
         ?>
